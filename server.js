1ca0384dfcaa9ed20d1b4e832a19e9e502277e6c
Dan Bushong <bushongdan@gmail.com>
2021-03-01 17:37:22 -0600

    try heroku again

--- server.js
+++ server.js
@@ -1,40 +1,30 @@
-const express = require("express");
-const logger = require("morgan");
-const mongoose = require("mongoose");
+const express = require('express');
+const logger = require('morgan');
+const mongoose = require('mongoose');
 
-const PORT = process.env.MONGODB_URI;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+//use logger
 app.use(logger("dev"));
 
+//parser
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.json());
 
+//use static files
 app.use(express.static("public"));
 
-mongoose.connect(
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fittrack", {useNewUrlParser: true});
 
-    process.env.MONGODB_URI,
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-        useFindAndModify: false
-    }
- 
-);
 
-require("./routes/api-routes")(app);
-require("./routes/html-routes")(app);
-//can prob get rid of this after first use
-// require('./seeders/seed');
+
+//use routes
+require('./routes/api-routes')(app)
+require('./routes/html-routes')(app)
+
 
 app.listen(PORT, () => {
-    console.log(`It is running on port ${PORT}..`);
-});
-
-
-
-
+    console.log(`App running on port ${PORT}..`);
+})
