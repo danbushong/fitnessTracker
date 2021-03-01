d49db72783b4cffd79f2607ff791f7ad563f85d2
Dan Bushong <bushongdan@gmail.com>
2021-03-01 17:42:28 -0600

    styles made correct

--- server.js
+++ server.js
@@ -6,22 +6,22 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-//use logger
+
 app.use(logger("dev"));
 
-//parser
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-//use static files
+
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fittrack", {useNewUrlParser: true});
 
 
 
-//use routes
 require('./routes/api-routes')(app)
+
 require('./routes/html-routes')(app)
 
 
