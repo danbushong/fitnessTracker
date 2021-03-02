8cd41d7408cd08a9670060eeed2d87d4bdc40919
Dan Bushong <bushongdan@gmail.com>
2021-03-01 20:28:23 -0600

    last try

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const logger = require('morgan');
 const mongoose = require('mongoose');
 
-const PORT = process.env.PORT || 27017;
+const PORT = process.env.MONGODB_URI || 27017;
 
 const app = express();
 
@@ -16,7 +16,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fittrack", {useNewUrlParser: true});
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true , useCreateIndex: true, useUnifiedTopology: true });
 
 
 
