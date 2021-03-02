90859664686fa9badeaed764e460ef65c8c4f7fe
Dan Bushong <bushongdan@gmail.com>
2021-03-01 18:09:57 -0600

    server attempt

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const logger = require('morgan');
 const mongoose = require('mongoose');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.MONGODB_URI;
 
 const app = express();
 
@@ -16,8 +16,17 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://dan-admin:Durango11790@cluster0.ufqj5.mongodb.net/fittracker?retryWrites=true&w=majority", {useNewUrlParser: true});
+const MongoClient = require('mongodb').MongoClient;
 
+const uri = "mongodb+srv://dan-admin:Durango11790>@cluster0.ufqj5.mongodb.net/fittracker?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
 
 
 require('./routes/api-routes')(app)
